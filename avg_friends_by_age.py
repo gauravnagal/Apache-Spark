48d2a986d162352934c2f01bb5ba9b1f591dd449
Gaurav Nagal <47132774+gauravnagal@users.noreply.github.com>
2020-06-21 22:29:45 -0400

    Add files via upload

---
+++ avg_friends_by_age.py
@@ -0,0 +1,44 @@
+'''
+To find average number of friends by age.
+Input data: id, name, age, number_of_friends
+'''
+
+from pyspark import SparkConf, SparkContext
+
+conf = SparkConf().setMaster("local").setAppName("AvgFriendsByAge")
+sc = SparkContext(conf = conf)
+
+# Parse (map) the input data
+def parseLine(line):
+    fields = line.split(',') # split line based on commas
+    age = int(fields[2]) # casting string to int value
+    numFriends = int(fields[3])
+    return (age, numFriends) # return key/value pair
+
+# create lines RDD that's calling text file on the SparkContext with our input data
+lines = sc.textFile("file:///Apache_Spark/Datasets/friendsandage.csv")
+
+# transforming lines RDD to rdd RDD. Creates key/value RDD
+rdd = lines.map(parseLine)
+
+# calling mapValues on numFriends and transform it, taking resulting RDD and calling reduceByKey on it and adds up all
+# values for each unique key, then sorts results by key
+totalByAge = rdd.mapValues(lambda x: (x, 1)).reduceByKey(lambda x, y: (x[0] + y[0], x[1] + y[1])).sortByKey()
+
+# average value by age
+avgByAge = totalByAge.mapValues(lambda x: x[0] / x[1])
+
+# print results
+results = avgByAge.collect()
+for result in results:
+    print(result)
+
+
+
+
+
+
+
+
+
+
